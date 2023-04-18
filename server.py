048aa860471f2d06c06cabee4899edd69b8cfb46
vitaliishchudlo <vitalik.shchudlo01@ukr.net>
2023-04-18 23:10:43 +0300

    First workable version

--- server.py
+++ server.py
@@ -13,60 +13,55 @@ LINK_PREFIX = 'api_v1/for-csgo-google-sheet'
 NEEDED_ARGUMENT_IN_LINK = 'skin_link'
 
 
-class Browser:
-    def __init__(self):
-        self.driver = None
-
-    def create_driver(self):
-        options = Options()
-        # options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
-        service = Service(ChromeDriverManager().install())
-        self.driver = webdriver.Chrome(service=service, options=options)
-
-    def get_driver(self):
-        if not self.driver:
-            self.create_driver()
-        return self.driver
-
-    def close(self):
-        self.driver.quit()
+def create_driver():
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.headless = False
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
+    return driver
 
 
 @app.route('/')
 def index():
     if not request.args.get(NEEDED_ARGUMENT_IN_LINK):
         return jsonify(error=f"You must specify a '{NEEDED_ARGUMENT_IN_LINK}' parameter in the link")
+
     skin_link = request.args.get(NEEDED_ARGUMENT_IN_LINK)
-    driver = Browser().get_driver()
+    skin_link = skin_link.replace(' ', '%20')
+
+    driver = create_driver()
     try:
-        res = driver.get(skin_link)
+        driver.get(skin_link)
         element_name = driver.find_element(By.ID, 'largeiteminfo_item_name')
-        element_price = driver.find_element(By.CLASS_NAME, 'market_commodity_order_summary')
+        name_of_skin_text = element_name.text
 
-        print('Link: ', skin_link)
-        print('Element name: ', element_name.text)
-
-        driver.execute_script("arguments[0].scrollIntoView();", element_price)
+        element_of_sales = driver.find_element(By.CLASS_NAME, 'market_commodity_order_summary')
+        driver.execute_script("arguments[0].scrollIntoView();", element_of_sales)
         time.sleep(1)
 
-        print('Element price: ', element_price.text)
-        print('Element price: ', element_price.text.split('$'))
-        print('Element price: ', element_price.text.split('$')[-1])
+        try:
+            lowest_price_text = element_of_sales.text.split('$')[-1]
+            count_of_sales_text = element_of_sales.text.split(' ')[0]
+        except Exception:
+            print('refreshing')
+            driver.refresh()
+            time.sleep(3)
+            driver.execute_script("arguments[0].scrollIntoView();", element_of_sales)
+            time.sleep(1)
+            lowest_price_text = element_of_sales.text.split('$')[-1]
+            count_of_sales_text = element_of_sales.text.split(' ')[0]
 
-
-        print('At the end of the method')
-        time.sleep(15)
-        return res
-    except Exception:
-        pass
+        return jsonify(title_of_skin=name_of_skin_text, lowest_price=lowest_price_text,
+                       count_of_sales=count_of_sales_text)
+    except Exception as err:
+        print('\n\n ERROR: ', err)
     finally:
-        driver.close()
-
+        driver.quit()
 
 
 if __name__ == '__main__':
-    print(
-        f'http://192.168.0.105:5000?{NEEDED_ARGUMENT_IN_LINK}=https://steamcommunity.com/market/listings/730/Revolution%20Case')
     app.run(host='0.0.0.0', debug=True)

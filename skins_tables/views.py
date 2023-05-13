1a1135cff21d271c9390315f73ababf6d9ea6563
vitaliishchudlo <vitalik.shchudlo01@ukr.net>
2023-05-13 15:39:54 +0300

    Tmp save

--- skins_tables/views.py
+++ skins_tables/views.py
@@ -1,11 +1,13 @@
-import time
+import asyncio
+import threading
 
 from django.shortcuts import render
 from django.urls import reverse
-import requests
 from .models import Skin
 import requests
 import json
+
+REFRESHING_PROCESS = False
 
 def index(request):
     button_url = reverse('admin:index')
@@ -20,7 +22,11 @@ def statistics(request):
     return render(request, 'statistics.html', context)
 
 
-def refresh(request):
+async def refreshing_skins_price(request):
+    # for x in range(10):
+    #     print(f'In the refreshing_skins_price METHOD {x}/10')
+    #     await asyncio.sleep(1)
+
     skins = Skin.objects.all()
     skin_names = [skin.name for skin in skins]
 
@@ -28,10 +34,11 @@ def refresh(request):
     base_link = f'http://18.193.224.198/?skin_name='
 
     for skin_name in skin_names:
+        print('The link: ', base_link + skin_name)
         response = requests.get(base_link + skin_name)
         if response.status_code == 200:
             data = json.loads(response.text)
-            skin_price = data.get('skin_price').replace('$','')
+            skin_price = data.get('skin_price').replace('$', '')
             skin_prices[skin_name] = skin_price
             print(skin_name)
             print(skin_price)
@@ -48,6 +55,23 @@ def refresh(request):
         except Skin.DoesNotExist:
             pass
 
-    import ipdb; ipdb.set_trace(context=5)
-    return render(request, 'refresh.html', {'refreshing_started': True})
-    # return render(request, 'refresh.html', {'refreshing_started': True})
+
+def start_refreshing_skins_price(request):
+    global REFRESHING_PROCESS
+    REFRESHING_PROCESS = True
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(refreshing_skins_price(request))
+    REFRESHING_PROCESS = False
+
+def refresh(request):
+
+    global REFRESHING_PROCESS
+    if REFRESHING_PROCESS:
+        return render(request, 'refresh.html', {'refreshing_started': False})
+    else:
+        thread = threading.Thread(target=start_refreshing_skins_price, args=(request,))
+        thread.start()
+        return render(request, 'refresh.html', {'refreshing_started': True})
+
+
